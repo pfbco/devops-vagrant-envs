dc7b8f443c817805c9646d3711a4b58ce53a3d3f
Pedro Coutinho <40065925+pfbco@users.noreply.github.com>
2020-09-16 22:24:52 +0100

    Added oh-my-zsh to the system provision

--- workstation/provisioning/system.sh
+++ workstation/provisioning/system.sh
@@ -20,3 +20,9 @@ dnf install nano zip wget git -y
 echo -e "\n> Change shell to ZSH..." 
 dnf install zsh util-linux-user -y
 chsh -s $(which zsh) vagrant
+
+echo -e "\n> Install oh-my-sh and custom plugins..."
+sudo -u vagrant sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-/home/vagrant/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-/home/vagrant/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+cp /vagrant/provisioning/zshrc.zsh-template /home/vagrant/.zshrc
